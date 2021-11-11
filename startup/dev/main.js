f21256d99d81e7cae5520e9578246c46feb8b078
Rolland42 <s.rolland@codeur.online>
2021-11-11 12:57:22 +0100

    entrenainement

---
+++ startup/dev/main.js
@@ -0,0 +1,9 @@
+
+function functionRetract() {
+    if (document.getElementById("menu").className === "burgerMenu") {
+      document.getElementById("menu").className += " responsive";
+    } else {
+      document.getElementById("menu").className = "burgerMenu";
+    }
+  }
+  
